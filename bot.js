b8b96245d863f252f3eb20b77dcc731548ecba51
Leo <leo.pan.duh@gmail.com>
2019-09-12 19:23:25 -0400

    add help command

--- bot.js
+++ bot.js
@@ -7,12 +7,27 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
+help = msg => {
+  msg.channel.send(
+    "Available commands:\n\
+  **!zones**\n\
+  - lists all supported time zones\n\
+  **!convert {time} {zone} {new zone} [yesterday/tomorrow/±day]**\n\
+  - converts a time from zone to new zone\n\
+  - uses date of first time zone provided\n\
+  - date can be incremented by specifying a number"
+  );
+};
+
 client.on("message", msg => {
   if (msg.content.substring(0, 1) === "!") {
     var args = msg.content.substring(1).split(" ");
     var cmd = args[0];
 
     switch (cmd) {
+      case "help":
+        help(msg);
+        break;
       case "convert":
         tt.convert(msg, args);
         break;
