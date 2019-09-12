5517a6603ff5c23960cd61d3fd2a330505bdcdf2
Leo <leo.pan.duh@gmail.com>
2019-09-12 19:22:05 -0400

    simplify converting process using momentjs

--- bot.js
+++ bot.js
@@ -1,13 +1,7 @@
+const tt = require("./time-zone");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const auth = require("./auth.json");
-const zones = require("./zones.json");
-
-convertErr = msg => {
-  msg.channel.send(
-    "Usage: **!convert {time} {zone} {new zone}**. \nFor a list of available time zones, enter **!zones**."
-  );
-};
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -19,58 +13,9 @@ client.on("message", msg => {
     var cmd = args[0];
 
     switch (cmd) {
-      case "help":
-        //TODO: Insert help manual here
-        break;
       case "convert":
-        // !convert {time} {zone} {zone}
-
-        // wrong arguments
-        if (args.length !== 4) {
-          convertErr(msg);
-          return;
-        }
-
-        var time = args[1].split(":");
-
-        // if input is not a unit of time
-        if (time[1].length < 2 || /\D/.test(time[0])) {
-          convertErr(msg);
-          return;
-        }
-
-        // if time zones don't exist (or aren't supported)
-        if (!zones.hasOwnProperty(args[2]) || !zones.hasOwnProperty(args[3])) {
-          convertErr(msg);
-          return;
-        }
-
-        var hour =
-          parseInt(time[0]) +
-          (time[1].substring(2).toUpperCase() === "PM" ? 12 : 0);
-
-        if (hour === 12 && time[1].substring(2).toUpperCase() === "AM") {
-          hour = 0;
-        }
-
-        var diff = zones[args[3]] - zones[args[2]];
-
-        hour = (hour + diff) % 24;
-
-        isPM = hour > 12;
-        if (isPM) hour -= 12;
-
-        var newTime =
-          hour.toString() +
-          ":" +
-          time[1].substring(0, 2) +
-          (isPM ? "PM" : "AM");
-
-        msg.channel.send(
-          `${args[1]} ${args[2]} => ${diff > 0 ? "+" : "-"}${Math.abs(
-            diff
-          )} hours => ${newTime} ${args[3]}.`
-        );
+        tt.convert(msg, args);
+        break;
     }
   }
 });
---
+++ time-zone.js
@@ -0,0 +1,83 @@
+const moment = require("moment");
+require("moment-timezone");
+const Discord = require("discord.js");
+const zones = require("./zones.json");
+
+convertErr = msg => {
+  msg.channel.send(
+    "Usage: **!convert {time} {zone} {new zone} [±day]**. \nFor a list of available time zones, enter **!zones**."
+  );
+};
+
+module.exports = {
+  // !convert {time} {zone} {zone} [±day]
+  convert: function(msg, args) {
+    // wrong arguments
+    if (args.length < 4) {
+      console.log("a");
+      convertErr(msg);
+      return;
+    }
+
+    // ensure time given is valid
+    if (!/^\d{1,2}:\d{2}(am|pm)$/i.test(args[1])) {
+      convertErr(msg);
+      return;
+    }
+
+    var time = args[1].split(":");
+
+    args[2] = args[2].toUpperCase();
+    args[3] = args[3].toUpperCase();
+
+    // if time zones don't exist (or aren't supported)
+    if (!zones.hasOwnProperty(args[2]) || !zones.hasOwnProperty(args[3])) {
+      convertErr(msg);
+      return;
+    }
+
+    // get time in local time zone
+    hour = parseInt(time[0]);
+    minute = parseInt(time[1].substring(0, 2));
+    isPM = time[1].substring(2).toUpperCase() === "PM";
+    addDay = 0;
+
+    if (isPM && hour != 12) hour += 12;
+    else if (!isPM && hour === 12) hour = 0;
+
+    if (hour > 24 || minute >= 60 || (hour >= 24 && minute > 0)) {
+      convertErr(msg);
+      return;
+    }
+
+    // change day if provided
+    if (args.length >= 5) {
+      if (args[4] === "yesterday") {
+        addDay = -1;
+      } else if (args[4] === "tomorrow") {
+        addDay = 1;
+      } else if (args[4] === "today") {
+        addDay = 0;
+      } else {
+        if (!/^[-+]?\d+$/.test(args[4])) {
+          convertErr(msg);
+          return;
+        }
+        addDay = parseInt(args[4]);
+      }
+    }
+
+    var date = moment()
+      .add(addDay, "days")
+      .tz(zones[args[2]]);
+    date.set({ hour: hour, minute: minute });
+
+    var date2 = date.clone().tz(zones[args[3]]);
+
+    msg.channel.send(
+      `${date.format("MMMM Do, YYYY | hh:mmA")} ${args[2]} = \n${date2.format(
+        "MMMM Do, YYYY | hh:mmA"
+      )} ${args[3]}`
+    );
+  }
+};
